fecbe8cd87b28f21ff2e7ef65b75691fbe4a7064
Bl0101 <yacine.mahdid@live.com>
2018-07-24 17:12:23 -0400

    Refactoring

--- EventsFinder/exploratory_scripts/batch_preprocessing.m
+++ EventsFinder/exploratory_scripts/batch_preprocessing.m
@@ -1,3 +1,12 @@
+% Batch_preprocessing as shown by the Quality Measure for TPS data document
+
 %% Process the data
 
-%% 
+%% Thresholding
+
+%% SQI
+
+%% Combination
+
+%% Exponential Decay Average Filter
+
