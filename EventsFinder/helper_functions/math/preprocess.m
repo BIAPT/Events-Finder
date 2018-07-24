3d4f31bb9ba8fed56fd5096809d7cfc89dd534ea
Bl0101 <yacine.mahdid@live.com>
2018-07-24 17:36:07 -0400

    structure preprocess function

--- EventsFinder/helper_functions/math/preprocess.m
+++ EventsFinder/helper_functions/math/preprocess.m
@@ -6,21 +6,50 @@ function [data_struct] = preprocess(app)
 %   Output
 %   data_struct: structure containing the cleaned data
 %   for every modality and both the participant and the care-giver
-
+%   For each modality we have the filtered signal with a corresponding
+%   signal quality array
     %%  Creating variables
     data_struct = struct();
-    data_struct.p.signal_quality = [];
-    data_struct.p.bvp_filt = [];
-    data_struct.p.hr_filt = [];
-    data_struct.p.sc_filt = [];
-    data_struct.p.temp_filt = [];
-
-    data_struct.c.signal_quality = [];
-    data_struct.c.bvp_filt = [];
-    data_struct.c.hr_filt = [];
-    data_struct.c.sc_filt = [];
-    data_struct.c.temp_filt = [];
+    % Participant
+    % Blood Volume Pulse
+    data_struct.p.bvp.time = [];
+    data_struct.p.bvp.filt = [];
+    data_struct.p.bvp.sq = [];
+    % Heart Rate 
+    data_struct.p.hr.time = [];
+    data_struct.p.hr.filt = [];
+    data_struct.p.hr.sq = [];
+    % Skin Conductance
+    data_struct.p.sc.time = [];
+    data_struct.p.sc.filt = [];
+    data_struct.p.sc.sq = [];
+    % Temperature
+    data_struct.p.temp.time = [];
+    data_struct.p.temp.filt = [];
+    data_struct.p.temp.sq = [];
     
+    % Care-giver
+    % Blood Volume Pulse
+    data_struct.c.bvp.time = [];
+    data_struct.c.bvp.filt = [];
+    data_struct.c.bvp.sq = [];
+    % Heart Rate
+    data_struct.c.hr.time = [];
+    data_struct.c.hr.filt = [];
+    data_struct.c.hr.sq = [];
+    % Skin Conductance
+    data_struct.c.sc.time = [];
+    data_struct.c.sc.filt = [];
+    data_struct.c.sc.sq = [];
+    % Temperature
+    data_struct.c.temp.time = [];
+    data_struct.c.temp.filt = [];
+    data_struct.c.temp.sq = [];
+    
+   %% Thresholding (TODO Wait for threshold from florian)
+   
+   %% Filtering (TODO use the exponential filter
+   
    
 end
 
