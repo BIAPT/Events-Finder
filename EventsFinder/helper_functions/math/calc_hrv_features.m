35dc4ce23bf97d5544f9f68fda2995030afe35b5
Bl0101 <yacine.mahdid@live.com>
2018-07-24 11:42:24 -0400

    cleaning

---
+++ EventsFinder/helper_functions/math/calc_hrv_features.m
@@ -0,0 +1,12 @@
+function [outputArg1,outputArg2] = calc_hrv_features(app,type)
+%CALC_HRV_FEATURES Calculate HRV features as shown in Unobtrusive Real-time
+%Heart Rate Variability Analysis for the Detection of Orthostatic Dysregulation
+%   Input
+%   app:
+%   type:
+%   Output
+
+
+
+end
+
