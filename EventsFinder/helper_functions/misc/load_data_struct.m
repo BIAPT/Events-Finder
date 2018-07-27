e0a36716211600d0f9033eff86c79aaccdb03894
Bl0101 <yacine.mahdid@live.com>
2018-07-27 15:06:02 -0400

    Refactored into functions

---
+++ EventsFinder/helper_functions/misc/load_data_struct.m
@@ -0,0 +1,8 @@
+function load_data_struct(app)
+%LOAD_DATA_STRUCT Will process the raw data to put them in Data in the right format
+%   Input
+%   app: contain the application public data
+
+
+end
+
