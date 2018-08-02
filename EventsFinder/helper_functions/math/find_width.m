1895f210c3f34eb901a0aed0e25fc9d867b6d77a
Bl0101 <yacine.mahdid@live.com>
2018-08-02 11:23:00 -0400

    Refactoring find_width

--- EventsFinder/helper_functions/math/find_width.m
+++ EventsFinder/helper_functions/math/find_width.m
@@ -1,15 +1,15 @@
 function [width] = find_width(diff,zero_cross_pts)
-%{
-%% Input (Example of inputs)
-diff = 1:1000;
-zero_cross_pts = 45;
-
-%% Output (This is the output)
-width = 1;
-%}
+%FIND_WIDTH % Find the width of a zero crossing point
+% Input
+% diff: array of first derivatives
+% zero_cross_pts: index of the zero_crossing point
+% 
+% Output
+% width: the width of this peak or through
 %% Variables
 left_border = zero_cross_pts - 1;
 right_border = zero_cross_pts + 1;
+width = 1; % default value
 
 left_sign = sign(diff(left_border));
 right_sign = sign(diff(right_border));
@@ -23,5 +23,3 @@ while(left_border > 0 && right_border < size(diff,1) ...
     left_border = left_border - 1;
     right_border = right_border + 1;
 end
-
-disp(['Width of this zero crossing pts is of size : ', num2str(width)]);
